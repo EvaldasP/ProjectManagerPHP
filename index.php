f04429bd93417cc842681e4a8afcf6071175077b
EvaldasP <72792658+EvaldasP@users.noreply.github.com>
2021-01-06 16:44:38 +0200

    Employees assigment to Project
    Project can have more than one  employee

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles.css">
@@ -23,17 +25,20 @@
     </header>
 
 
-    <table class="table table-striped table-dark">
+    <table class="table table-hover table-dark">
         <thead>
             <tr>
                 <th scope="col">ID</th>
-                <th scope="col">Name</th>
+
                 <?php
                 if ($_GET['path'] == "employee") {
+                    echo "<th scope=col>Vardas</th>";
                     echo "<th scope=col> Projektai</th>";
                 } else if ($_GET['path'] == "projects") {
+                    echo "<th scope=col>Projektas</th>";
                     echo "<th scope=col> Darbuotojai</th>";
                 } else {
+                    echo "<th scope=col>Vardas</th>";
                     echo "<th scope=col> Projektai</th>";
                 }
                 ?>
@@ -55,9 +60,6 @@
                 die("Connection failed: " . mysqli_connect_error());
             }
 
-
-
-
             //logika kuri pasikeitus path'ui keicia lentele kuri spausdinama
 
             if ($_GET['path'] == "employee" || $_GET['path'] == "") {
@@ -77,13 +79,19 @@
                     }
                 }
             } else if ($_GET['path'] == "projects") {
-                $sql = "SELECT id, name FROM projectmanager.projects";
+                $sql =  "SELECT projects.id as numeris, projects.name as projektas , GROUP_CONCAT(employee.name SEPARATOR', ') as vardas FROM projects
+                        LEFT JOIN employee ON employee.project_id = projects.id 
+                        GROUP BY projektas
+                        ORDER BY numeris;  
+                        ";
+
                 $result = mysqli_query($conn, $sql);
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) {
                         echo "<tr>
-                        <td> $row[id]</td>
-                        <td> $row[name]</td>
+                        <td> $row[numeris]</td>
+                        <td> $row[projektas]</td>
+                        <td> $row[vardas] </td>
                         </tr>";
                     }
                 }
